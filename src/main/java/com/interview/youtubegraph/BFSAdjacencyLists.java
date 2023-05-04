48ffcec0cf04e51ad270c3996d777529f75fa8ce
Jeevasimba <onestack991@gmail.com>
2023-05-04 15:55:23 +0530

    correcting the code

--- src/main/java/com/interview/youtubegraph/BFSAdjacencyLists.java
+++ src/main/java/com/interview/youtubegraph/BFSAdjacencyLists.java
@@ -1,94 +1,56 @@
 package com.interview.youtubegraph;
 
 import java.util.*;
-
 class bfs {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V,
+                                         ArrayList<ArrayList<Integer>> adj) {
 
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
-
-        ArrayList<Integer> bfs = new ArrayList<>();
-
-        boolean[] visited = new boolean[V];
-
-        Queue<Integer> q =new LinkedList<>();
+        ArrayList < Integer > bfs = new ArrayList < > ();
+        boolean vis[] = new boolean[V];
+        Queue < Integer > q = new LinkedList < > ();
 
         q.add(0);
-        visited[0] = true;
+        vis[0] = true;
 
-        while (!q.isEmpty()){
+        while (!q.isEmpty()) {
+            Integer node = q.poll();
+            bfs.add(node);
 
-            int ans = q.poll();
-
-            bfs.add(ans);
-
-            for (int i = 6; i < adj.get(ans).size() ; i++) {
-                if(!visited[adj.get(ans).get(i)]){
-                    visited[adj.get(ans).get(i)] = true;
-                    q.add(adj.get(ans).get(i));
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            for (Integer it: adj.get(node)) {
+                if (vis[it] == false) {
+                    vis[it] = true;
+                    q.add(it);
                 }
-
             }
-
         }
 
         return bfs;
-
     }
-
-    // Function to return Breadth First Traversal of given graph.
-//    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-//
-//
-//
-//
-//        ArrayList < Integer > bfs = new ArrayList < > ();
-//        boolean vis[] = new boolean[V];
-//        Queue < Integer > q = new LinkedList < > ();
-//
-//        q.add(0);
-//        vis[0] = true;
-//
-//        while (!q.isEmpty()) {
-//            Integer node = q.poll();
-//            bfs.add(node);
-//
-//            // Get all adjacent vertices of the dequeued vertex s
-//            // If a adjacent has not been visited, then mark it
-//            // visited and enqueue it
-//            for (Integer it: adj.get(node)) {
-//                if (vis[it] == false) {
-//                    vis[it] = true;
-//                    q.add(it);
-//                }
-//            }
-//        }
-//
-//        return bfs;
-//    }
 
     public static void main(String args[]) {
 
         ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             adj.add(new ArrayList < > ());
         }
-        adj.get(9).add(8);
-        adj.get(8).add(9);
-        adj.get(9).add(7);
-        adj.get(7).add(9);
-        adj.get(8).add(6);
-        adj.get(6).add(8);
-        adj.get(8).add(3);
-        adj.get(3).add(8);
+        adj.get(0).add(1);
+        adj.get(1).add(0);
+        adj.get(0).add(4);
+        adj.get(4).add(0);
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+        adj.get(1).add(3);
+        adj.get(3).add(1);
 
         bfs sl = new bfs();
-        ArrayList < Integer > ans = sl.bfsOfGraph(10, adj);
+        ArrayList < Integer > ans = sl.bfsOfGraph(5, adj);
         int n = ans.size();
         for(int i = 0;i<n;i++) {
             System.out.print(ans.get(i)+" ");
         }
-
     }
-
 }
