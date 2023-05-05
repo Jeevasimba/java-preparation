2937284b44ce4e0979262f5645e414ab368ff147
Jeevasimba <onestack991@gmail.com>
2023-05-05 10:28:22 +0530

    gitignore

---
+++ src/main/java/com/interview/youtubegraph/Biportitgraphbfs.java
@@ -0,0 +1,13 @@
+package com.interview.youtubegraph;
+
+import java.util.ArrayList;
+
+public class Biportitgraphbfs {
+
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        // Code here
+        return false;
+    }
+
+}
